7e7e0740bfdcb67f3b4c9be096e4ef0f02be7108
Elijan Mastnak <git@ejmastnak.com>
2023-09-09 23:03:11 +0200

    feat: add CompoundFigureController boilerplate

---
+++ app/Http/Controllers/CompoundFigureController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CompoundFigure;
+use Illuminate\Http\Request;
+
+class CompoundFigureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CompoundFigure $compoundFigure)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CompoundFigure $compoundFigure)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CompoundFigure $compoundFigure)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CompoundFigure $compoundFigure)
+    {
+        //
+    }
+}
