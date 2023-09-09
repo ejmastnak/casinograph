382890b69dc8a6db55f8338a970c629f57a71414
Elijan Mastnak <git@ejmastnak.com>
2023-09-09 13:32:06 +0200

    Initial commit
    Includes Model/Controller/Migration/Seeder boilerplate for Position and
    Figure

---
+++ app/Http/Controllers/FigureController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Figure;
+use Illuminate\Http\Request;
+
+class FigureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Figure $figure)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Figure $figure)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Figure $figure)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Figure $figure)
+    {
+        //
+    }
+}
