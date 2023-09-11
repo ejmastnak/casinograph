d13e439e493731b2c02ab55de00c00abd736a2bf
Elijan Mastnak <git@ejmastnak.com>
2023-09-11 22:07:43 +0200

    feat: add authorization policies
    Adds user role columns to users table and authorization policies for
    Figure and Position.

---
+++ app/Policies/FigurePolicy.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Figure;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class FigurePolicy
+{
+
+    /**
+     * Perform pre-authorization checks.
+     */
+    public function before(User $user, string $ability): bool|null
+    {
+        if ($user->is_admin) return true;
+        return null;
+    }
+
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Figure $figure): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->can_create;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Figure $figure): bool
+    {
+        return $user->can_update && $figure->user_id === $user->id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Figure $figure): bool
+    {
+        return $user->can_delete && $figure->user_id === $user->id;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Figure $figure): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Figure $figure): bool
+    {
+        //
+    }
+}
---
+++ app/Policies/PositionPolicy.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Position;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class PositionPolicy
+{
+
+    /**
+     * Perform pre-authorization checks.
+     */
+    public function before(User $user, string $ability): bool|null
+    {
+        if ($user->is_admin) return true;
+        return null;
+    }
+
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Position $position): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->can_create;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Position $position): bool
+    {
+        return $user->can_update && $position->user_id === $user->id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Position $position): bool
+    {
+        return $user->can_delete && $position->user_id === $user->id;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Position $position): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Position $position): bool
+    {
+        //
+    }
+}
