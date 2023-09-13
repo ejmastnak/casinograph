04c42951ffdaec216b36941f8b0316285aa07629
Elijan Mastnak <git@ejmastnak.com>
2023-09-13 23:15:01 +0200

    fix: add missing flash prop to HandleInertiaRequest

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -32,6 +32,9 @@ class HandleInertiaRequests extends Middleware
     {
         return [
             ...parent::share($request),
+            'flash' => [
+                'message' => fn () => $request->session()->get('message')
+            ],
             'auth' => [
                 'user' => $request->user(),
             ],
