89d1d0bb6c14f7b5d33bdd290ed544203a22e756
Elijan Mastnak <git@ejmastnak.com>
2023-09-15 01:23:46 +0200

    refactor: switch to table to display positions

--- resources/js/Pages/Positions/Index.vue
+++ resources/js/Pages/Positions/Index.vue
@@ -32,24 +32,51 @@ export default {
       </SecondaryLink>
     </div>
 
-    <ul class="mt-6 border border-gray-200 rounded-md shadow-sm">
-      <li
-        v-for="position in positions"
-        class="flex px-2 py-1 border-b border-gray-200"
-      >
-        <MyLink :href="route('positions.show', position.id)">
-          {{position.name}}
-        </MyLink>
-        <div class="flex items-center ml-auto">
-          <MyLink :href="route('positions.edit', position.id)">
-            <PencilSquareIcon class="text-gray-500 h-5 w-5"/>
-          </MyLink>
-          <MyLink as="button" method="delete" :href="route('positions.destroy', position.id)" class="ml-1">
-            <TrashIcon class="text-gray-500 h-5 w-5"/>
-          </MyLink>
-        </div>
-      </li>
-    </ul>
+    <table class="mt-6 sm:table-fixed w-full text-sm sm:text-base text-left">
+      <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+        <tr>
+          <th scope="col" class="px-6 py-2 w-8/12 bg-blue-100">
+            Name
+          </th>
+          <th scope="col" class="px-6 py-2 w-3/12 bg-blue-200">
+            Family
+          </th>
+          <!-- For trash and edit icons -->
+          <th scope="col" class="bg-blue-100" />
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="position in positions" :key="position.id"
+          class="bg-white border-b"
+        >
+          <!-- Name -->
+          <td scope="row" class="px-5 py-2">
+            <MyLink :href="route('positions.show', position.id)">
+              {{position.name}}
+            </MyLink>
+          </td>
+          <!-- PositionFamily -->
+          <td class="px-6 py-2 text-gray-700">
+            {{position.position_family?.name}}
+          </td>
+          <!-- Delete/Edit -->
+          <td>
+            <div class="flex items-center">
+              <MyLink :href="route('positions.edit', position.id)">
+                <PencilSquareIcon class="text-gray-500 h-5 w-5"/>
+              </MyLink>
+              <MyLink as="button" method="delete" :href="route('positions.destroy', position.id)">
+                <TrashIcon class="text-gray-500 h-5 w-5"/>
+              </MyLink>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+
+
 
   </div>
 </template>
