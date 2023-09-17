945363a7163495171112517c0a7dc892a08c410c
Elijan Mastnak <git@ejmastnak.com>
2023-09-17 21:49:17 +0200

    feat: support errors in FlashMessage

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -33,7 +33,8 @@ class HandleInertiaRequests extends Middleware
         return [
             ...parent::share($request),
             'flash' => [
-                'message' => fn () => $request->session()->get('message')
+                'message' => fn () => $request->session()->get('message'),
+                'error' => fn () => $request->session()->get('error'),
             ],
             'auth' => [
                 'user' => $request->user(),
--- resources/js/Components/FlashMessage.vue
+++ resources/js/Components/FlashMessage.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { InformationCircleIcon } from '@heroicons/vue/24/outline'
+import { InformationCircleIcon, ExclamationCircleIcon } from '@heroicons/vue/24/outline'
 import { router, usePage } from '@inertiajs/vue3'
 import {watch, ref, computed } from 'vue'
 
@@ -22,32 +22,54 @@ watch(() => props.flash, () => {
       show.value = false
     }, 3000);
   }
+  if (props.flash.error) {
+    show.value = true;
+    setTimeout(() => {
+      props.flash.error = null
+      show.value = false
+    }, 10000);
+  }
 });
 
 </script>
 
 <template>
-  <Transition>
-    <div
-      v-show="flash.message && show"
-      @click="hide"
-      class="absolute inset-x-0 mx-auto  flex items-center text-sm text-gray-800 bg-blue-50 px-4 py-3 rounded-b-md border-b-2 border-blue-400 z-50"
-    >
-      <InformationCircleIcon class="w-5 h-5 text-gray-600" />
-      <span class="ml-1 translate-y-px">{{ flash.message }}</span>
-    </div>
-  </Transition>
+  <div class="">
+    <Transition>
+      <div
+        v-show="flash.message && show"
+        @click="hide"
+        class="absolute inset-x-0 mx-auto  flex items-center text-sm text-gray-800 bg-blue-50 px-4 py-3 rounded-b-md border-b-2 border-blue-400 z-50"
+      >
+        <InformationCircleIcon class="w-5 h-5 text-gray-600 shrink-0" />
+        <span class="ml-1 translate-y-px">{{ flash.message }}</span>
+      </div>
+    </Transition>
+
+    <Transition>
+      <div
+        v-show="flash.error && show"
+        @click="hide"
+        class="absolute inset-x-0 mx-auto  flex items-center text-sm text-gray-800 bg-red-200 px-4 py-3 rounded-b-md border-b-2 border-red-400 z-50"
+      >
+        <ExclamationCircleIcon class="w-6 h-6 text-gray-600 shrink-0" />
+        <span class="ml-2 translate-y-px">{{ flash.error }}</span>
+      </div>
+    </Transition>
+
+  </div>
+
 </template>
 
 <style>
 .v-enter-active {
-  transition: opacity 0.1s ease;
+transition: opacity 0.1s ease;
 }
 .v-leave-active {
-  transition: opacity 0.5s ease;
+transition: opacity 0.5s ease;
 }
 .v-enter-from,
 .v-leave-to {
-  opacity: 0;
+opacity: 0;
 }
 </style>
