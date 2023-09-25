d70915d4ae05aa63528c66e8175916fb7ad2ab62
Elijan Mastnak <git@ejmastnak.com>
2023-09-25 16:11:30 +0200

    style: make some miscellaneous tweaks to home page

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -12,13 +12,14 @@ export default {
 </script>
 
 <template>
-  <div class="pb-8">
+  <div>
     <Head title="Home" />
 
     <h1 class="text-2xl text-gray-800">CasinoGraph</h1>
 
     <div class="mt-2 max-w-xl">
-      This site is a fusion of Cuban Casino and graph theory: you're seeing a representation of Casino as a cyclic directed graph in which the figures (edges) connect the positions (vertices).
+      This site approaches Cuban Casino from a computer science perspective:
+      you're seeing a representation of Casino as a cyclic directed graph in which the figures (edges) connect the positions (vertices).
       <a href="#more-info" class="p-px rounded-md text-blue-500 hover:text-blue-600 hover:underline focus:outline-none focus:ring-2 focus:ring-blue-700" >
         More info lower on this page.
       </a>
@@ -32,27 +33,23 @@ export default {
       <h2 class="text-xl text-gray-700" id="more-info">About the graph</h2>
       <ul class="mt-2 list-disc space-y-1">
         <li>
-          <span class="font-semibold">It's clickable!</span>
+          <span class="font-medium">It's clickable!</span>
           Click on any figure or position to learn more.
         </li>
         <li>
-          <span class="font-semibold">Simple figures:</span>
+          <span class="font-medium">Simple figures:</span>
           Only simple figures (one dancer's eight-count) are shown.
           Want to see compound figures?
           Visit the <MyLink :colored="true" :href="route('figures.index')">figures page</MyLink> and use the filter to show only compound figures.
         </li>
         <li>
-          <span class="font-semibold">One figure per position pair:</span>
+          <span class="font-medium">One figure per position pair:</span>
           Only one figure is drawn between each pair of positions—this is intentional, to avoid overcrowding the graph with parallel figures.
         </li>
         <li>
-          <span class="font-semibold">Refresh the page for new parallel figures:</span>
+          <span class="font-medium">Refresh the page for new parallel figures:</span>
           Of course, two positions are often connected by multiple figures (for example, Vacílala, Enchufa, and Dile que sí all take you from the Open position to Caida), so showing only one figure per position pair will leave other figures out.
           To keep things interesting, you can refresh the page to display a new (randomly chosen) set of parallel figures.
-        </li>
-        <li>
-          <span class="font-semibold">Graphviz:</span>
-          The graph is drawn using <MyLink :colored="true" href="https://graphviz.org/">Graphviz</MyLink>. Thank you, Graphviz!
         </li>
       </ul>
     </div>
@@ -65,7 +62,7 @@ export default {
             <template #question>Can I add new positions and figures?</template>
             <template #answer>
               That would be cool, please do!
-              You'll need an authorized account—send me an email at <a href="mailto:admin@ejmastnak.com" class="p-px rounded-md text-blue-500 hover:text-blue-600 hover:underline focus:outline-none focus:ring-2 focus:ring-blue-700" >
+              You'll need an account—send me an email at <a href="mailto:admin@ejmastnak.com" class="p-px rounded-md text-blue-500 hover:text-blue-600 hover:underline focus:outline-none focus:ring-2 focus:ring-blue-700" >
                 admin@ejmastnak.com
               </a> (in English, Slovene, or Spanish) and I'll set you up.
             </template>
@@ -87,7 +84,7 @@ export default {
             <template #question>There's no videos!</template>
             <template #answer>
               I know.
-              There definitely <span class="font-semibold">should</span> be be videos—dance is inherently visual and you can only get so far with plain-text descriptions.
+              There definitely <span class="italic">should</span> be be videos—dance is inherently visual and you can only get so far with plain-text descriptions.
               But that's a project beyond the scope of what I can currently commit to.
             </template>
           </QuestionAndAnswer>
@@ -131,9 +128,8 @@ export default {
           <QuestionAndAnswer>
             <template #question>What, you're storing a graph in a relational database?!</template>
             <template #answer>
-              Yeah, definitely not well-suited to efficient graph operations, but very well-suited to web dev.
-              I entertained the idea of using a graph database... but then realized this app only ever queries one level of adjacent vertices per web request,
-              and doesn't actually require any of the classic graph algorithms (finding shortest paths, detecting cycles, etc.) that a graph database is meant to support.
+              I entertained the idea of using a graph-oriented database... but then realized this app generally queries only one level of adjacent vertices per web request,
+              and doesn't actually traverse the graph or require any of the classic graph algorithms (finding shortest paths, etc.) that a graph database efficiently supports.
               And so the convenience of using a relational database in the web development ecosystem justifies any slight performance penalty thus incurred.
               (The one exception is drawing the graph on this page, but Graphviz builds its own representation of the graph when doing that anyway.)
             </template>
@@ -146,7 +142,7 @@ export default {
             <template #answer>
               <p>
                 The basic idea is to represent Casino as a directed graph in which positions are vertices and figures are edges.
-                The graph is cyclic (obviously) and permits both
+                The graph is cyclic (obviously—otherwise the dance would be over after a few figures) and permits both
                 self-referencing edges (e.g., Guapea points from the Open position back to the Open position, Exhibela from Caida back to Caida, etc.) and parallel edges (e.g., Vacílala, Enchufa, and Dile que sí all take you from the Open position to Caida).
               </p>
 
@@ -159,7 +155,7 @@ export default {
                 </li>
 
                 <li>
-                  I've chosen to uniquely identify a simple figure by the <span class="font-semibold">combination of</span> its name, starting position, and ending position. This leads to overloaded figures names (e.g. this app has over ten figures called Enchufa), but I see this more as a feature than a bug—it shows the underlying simplicity of Casino figures (it's all just variations and combinations of a handful of foundational figures),
+                  I've chosen to uniquely identify a simple figure by the <span class="italic">combination of</span> its name, starting position, and ending position. This leads to overloaded figures names (e.g. this app has over ten figures called Enchufa), but I see this more as a feature than a bug—it shows the underlying simplicity of Casino figures (it's all just variations and combinations of a handful of foundational figures),
                   and we avoid all the awkward figure names that would result from requiring unique figure names (imagine "Enchufa from Open to Caida", "Enchufa from Uno to Sombrero", "Right-handed enchufa from Open to Caida", "Double-handed enchufa from Open to Caida", etc.).
                 </li>
 
@@ -180,7 +176,6 @@ export default {
         </li>
       </ul>
     </div>
-
 
   </div>
 </template>
