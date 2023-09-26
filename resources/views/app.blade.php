bd35bf8a99a24e20f07c445ac032f389d77f59d5
Elijan Mastnak <git@ejmastnak.com>
2023-09-26 14:37:12 +0200

    feat: add noscript warning about requiring JavaScript

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,6 +15,14 @@
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
+    <noscript>
+        <div>
+            <h2 class="text-sm">This web site requires JavaScript.</h2>
+            <p style="max-width: 42rem; line-height: 1.5; color: rgb(31 41 55);">
+                I'm sorry that I have to require this—I tend to disable JavaScript myself—but I'm using a JavaScript framework (Vue.js) for this site and there's no way around it. I promise I won't do anything yucky; if you feel inspired to check for yourself, you can find the source code <a href="https://github.com/ejmastnak/casinograph">on GitHub</a>.
+            </p>
+        </div>
+    </noscript>
     <body class="font-sans antialiased">
         @inertia
     </body>
