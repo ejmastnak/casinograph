cd4c9a3d7f3c3f2fe746caa06c51b54d04e4a6ec
Elijan Mastnak <git@ejmastnak.com>
2023-09-26 17:54:34 +0200

    style: tweak spacing on noscript message

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,8 +16,8 @@
         @inertiaHead
     </head>
     <noscript>
-        <div>
-            <h2 style="font-size: 1.5em; margin-bottom: 0.83em; font-weight: bold;">This web site requires JavaScript.</h2>
+        <div style="padding: 0.5rem;">
+            <h2 style="font-size: 1.2em; margin-bottom: 0.83em; font-weight: bold;">This website requires JavaScript.</h2>
             <p style="max-width: 42rem; line-height: 1.5; color: rgb(31 41 55);">
                 I'm sorry that I have to require this—I tend to disable JavaScript myself—but I'm using a JavaScript framework (Vue.js) for this site and there's no way around it. I promise I won't do anything yucky; if you feel inspired to check for yourself, you can find the source code
                 <a style="color: rgb(7 99 235); text-decoration-line: underline;" href="https://github.com/ejmastnak/casinograph">on GitHub</a>.
