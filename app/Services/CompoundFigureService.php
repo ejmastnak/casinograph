460f474f1cb6b414f36e804cf126d8f04527083f
Elijan Mastnak <git@ejmastnak.com>
2024-03-25 10:55:29 +0100

    feat: add store and update methods to CompoundFigureService
    I forgot about CompoundFigures when I first created CRUD services in
    commit 0f652ec. This commit adds store and update methods to
    CompoundFigureService.

--- app/Services/CompoundFigureService.php
+++ app/Services/CompoundFigureService.php
@@ -2,6 +2,7 @@
 namespace App\Services;
 
 use App\Models\CompoundFigure;
+use App\Models\CompoundFigureFigure;
 use App\Models\Figure;
 use App\Models\FigureFamily;
 use App\Exceptions\FigureUpdateCorruptsCompoundFigureException;
@@ -9,6 +10,111 @@ use Illuminate\Support\Facades\Log;
 
 class CompoundFigureService
 {
+
+    public function storeCompoundFigure(array $data, int $userId) {
+        $compoundFigure = null;
+        try {
+            DB::transaction(function () use ($data, $userId, &$compoundFigure) {
+
+                $figureFamilyId = null;
+                if (isset($data['figure_family']) && $data['figure_family']['id']) {
+                    $figureFamilyId = $data['figure_family']['id'];
+                } else if (isset($data['figure_family']) && is_null($data['figure_family']['id'])) {
+                    $figureFamilyId = FigureFamily::create([
+                        'name' => $data['figure_family']['name'],
+                        'user_id' => $userId,
+                    ])->id;
+                }
+
+                $fromPositionId = Figure::find($data['figure_ids'][0])->from_position_id;
+                $toPositionId = Figure::find($data['figure_ids'][count($data['figure_ids']) - 1])->to_position_id;
+
+                $compoundFigure = CompoundFigure::create([
+                    'name' => $data['name'],
+                    'description' => $data['description'],
+                    'weight' => $data['weight'] ? $data['weight'] : config('misc.default_figure_weight'),
+                    'figure_family_id' => $figureFamilyId,
+                    'from_position_id' => $fromPositionId,
+                    'to_position_id' => $toPositionId,
+                    'user_id' => $userId,
+                ]);
+
+                foreach ($data['figure_ids'] as $idx=>$figureId) {
+                    CompoundFigureFigure::create([
+                        'figure_id' => $figureId,
+                        'compound_figure_id' => $compoundFigure->id,
+                        'seq_num' => $idx + 1,
+                        'is_final' => $idx === count($data['figure_ids']) - 1,
+                        'user_id' => $userId,
+                    ]);
+                }
+            });
+        } catch (\Exception $e) {
+            if (\App::environment('local')) throw $e;
+            Log::error($e);
+            return null;
+        }
+        return $compoundFigure->id;
+    }
+
+    public function updateCompoundFigure(array $data, CompoundFigure $compoundFigure): ?int
+    {
+        try {
+            DB::transaction(function () use ($data, &$compoundFigure) {
+
+                $previousFigureFamily = $compoundFigure->figure_family;
+
+                $figureFamilyId = null;
+                if (isset($data['figure_family']) && $data['figure_family']['id']) {
+                    $figureFamilyId = $data['figure_family']['id'];
+                } else if (isset($data['figure_family']) && is_null($data['figure_family']['id'])) {
+                    $figureFamilyId = FigureFamily::create([
+                        'name' => $data['figure_family']['name'],
+                        'user_id' => $userId,
+                    ])->id;
+                }
+
+                $fromPositionId = Figure::find($data['figure_ids'][0])->from_position_id;
+                $toPositionId = Figure::find($data['figure_ids'][count($data['figure_ids']) - 1])->to_position_id;
+
+                $compoundFigure->update([
+                    'name' => $data['name'],
+                    'description' => $data['description'],
+                    'weight' => $data['weight'] ? $data['weight'] : config('misc.default_figure_weight'),
+                    'figure_family_id' => $figureFamilyId,
+                    'from_position_id' => $fromPositionId,
+                    'to_position_id' => $toPositionId,
+                ]);
+
+                // I'm just deleting old CompoundFigureFigures and creating new
+                // ones on each update, doesn't seem worth the extra complexity of
+                // going through create-new-update-existing-delete-stale.
+                foreach ($compoundFigure->compound_figure_figures as $cff) $cff->delete();
+                foreach ($data['figure_ids'] as $idx=>$figureId) {
+                    CompoundFigureFigure::create([
+                        'figure_id' => $figureId,
+                        'compound_figure_id' => $compoundFigure->id,
+                        'seq_num' => $idx + 1,
+                        'is_final' => $idx === count($data['figure_ids']) - 1,
+                        'user_id' => $user ? $user->id : null,
+                    ]);
+                }
+
+                // If this update will orphan a figure family, delete it.
+                if ($previousFigureFamily) {
+                    if (($figureFamilyId !== $previousFigureFamily->id) && Figure::where('figure_family_id', $previousFigureFamily->id)->count() === 0 && CompoundFigure::where('figure_family_id', $previousFigureFamily->id)->count() === 0) {
+                        $previousFigureFamily->delete();
+                    }
+                }
+            });
+        } catch (\Exception $e) {
+            if (\App::environment('local')) throw $e;
+            Log::error($e);
+            return null;
+        }
+        return $compoundFigure->id;
+    }
+
     public function deleteCompoundFigure(CompoundFigure $compoundFigure): array
     {
         $success = false;
