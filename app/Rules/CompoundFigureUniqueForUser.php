506dfc9542e682355d6488b4f3767b173fc08d31
Elijan Mastnak <git@ejmastnak.com>
2024-04-17 21:50:05 +0200

    feat: add unique rule for CompoundFigures
    Creates a validation rule CompoundFigureUniqueForUser that ensures every
    CompoundFigure has a unique combination of user id, name, and figure
    sequence.

--- app/Rules/CompoundFigureUniqueForUser.php
+++ app/Rules/CompoundFigureUniqueForUser.php
@@ -8,8 +8,7 @@ use Illuminate\Contracts\Validation\ValidationRule;use Illuminate\Contracts\Vali
 use Illuminate\Support\Facades\Auth;
 
 /**
- *  Ensures the compound figure has a unique combination of name,
- *  from_position_id, and to_position_id for the given user.
+ *  Ensures the compound figure has a unique combination of name and figures.
  */
 class CompoundFigureUniqueForUser implements ValidationRule, DataAwareRule
 {
@@ -39,17 +38,19 @@ class CompoundFigureUniqueForUser implements ValidationRule, DataAwareRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        $from_position_id = $this->data['figure_ids'][0];
-        $to_position_id = $this->data['figure_ids'][count($this->data['figure_ids']) - 1];
-
-        $figure = CompoundFigure::where([
+        $candidates = CompoundFigure::where([
             ['name', 'like', $this->data['name']],
-            ['from_position_id', '=', $from_position_id],
-            ['to_position_id', '=', $to_position_id],
             ['user_id', '=', Auth::id()],
-        ])->first();
-        if (!is_null($figure) && $figure->id !== $this->data['id']) {
-            $fail("A figure with this combination of name, from position, and to position already exists. Change this figure's the name, from position, or to position.");
+        ])->get();
+
+        // Check for a compound figure with an identical figure sequence
+        foreach ($candidates as $candidate) {
+            $candidateFigureIds = array_map(function($cff) {
+                return $cff['figure_id'];
+            }, $candidate->compound_figure_figures->toArray());
+            if ($candidateFigureIds === $this->data['figure_ids']) {
+                $fail("A figure with this combination of name and figure sequence already exists. Change this figure's the name or figure sequence.");
+            }
         }
     }
 }
